6603b13c9eca36d5fae92231ef2fdab693f018fd
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-08-12 23:16:55 +0300

    Apply PER codestyle for closures

--- src/Header.php
+++ src/Header.php
@@ -59,17 +59,12 @@ final class Header implements HeaderInterface
             $ttfVersion = Version::create($ttfVersion->toString());
         }
 
-        // phpcs:disable
-        $pre->define('_SDL_TTF_VERSION_GTE', static fn (string $expected): bool =>
-            \version_compare($ttfVersion->toString(), $expected, '>=')
-        );
-        $pre->define('_SDL_VERSION_GTE', static fn (string $expected): bool =>
-            \version_compare($sdlVersion->toString(), $expected, '>=')
-        );
-        $pre->define('SDL_VERSION_ATLEAST', static fn (string $a, string $b, string $c): bool =>
-            \version_compare($sdlVersion->toString(), \sprintf('%d.%d.%d', $a, $b, $c), '>=')
-        );
-        // phpcs:enable
+        $pre->define('_SDL_TTF_VERSION_GTE', static fn (string $expected): bool
+            => \version_compare($ttfVersion->toString(), $expected, '>='));
+        $pre->define('_SDL_VERSION_GTE', static fn (string $expected): bool
+            => \version_compare($sdlVersion->toString(), $expected, '>='));
+        $pre->define('SDL_VERSION_ATLEAST', static fn (string $a, string $b, string $c): bool
+            => \version_compare($sdlVersion->toString(), \sprintf('%d.%d.%d', $a, $b, $c), '>='));
 
         $pre->add('SDL.h', self::SDL_H);
         $pre->add('SDL_version.h', '');
